269a8301d7f561e38c829478c8c8b87061d06f8b
Krasilnikov30 <79829503+KRASILNIKOV30@users.noreply.github.com>
2022-01-30 20:14:33 +0300

    Подключил карты

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import DeckGL from '@deck.gl/react';
+import {LineLayer} from '@deck.gl/layers';
+const INITIAL_VIEW_STATE = {
+    longitude: -122.41669,
+    latitude: 37.7853,
+    zoom: 13,
+    pitch: 0,
+    bearing: 0
+};
+
+
+
 const App = () => {
     return (
         <h1>
