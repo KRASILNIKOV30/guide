e91fa8c4ad8cf477a54ea11d005881abff6d59b5
Krasilnikov30 <79829503+KRASILNIKOV30@users.noreply.github.com>
2022-01-30 20:48:58 +0300

    Добавил карты

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
+import DeckGL from '../node_modules/@deck.gl/react';
+import {LineLayer} from '../node_modules/@deck.gl/layers';
+import {StaticMap} from 'react-map-gl';
+import {MapView, FirstPersonView} from '../node_modules/@deck.gl/core';
+
 const INITIAL_VIEW_STATE = {
-    longitude: -122.41669,
-    latitude: 37.7853,
+    longitude: 47.8896,
+    latitude:  56.6418,
     zoom: 13,
     pitch: 0,
     bearing: 0
 };
 
+const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoia3Jhc2lsbmlrb3YzMCIsImEiOiJja3owODh1OGwwMjE2MnBwcXlkNGpwZ281In0.Ep9Yn349-UgsCl8SPEroHQ';
 
+const data = [
+    {sourcePosition: [56.6418, 47.8896], targetPosition: [56.6418, 47.8896]}
+];
 
 const App = () => {
-    return (
-        <h1>
-            Hello
-        </h1>
-    )
+    const layers = [
+        new LineLayer({id: 'line-layer', data})
+      ];
+    
+    return ( 
+        <DeckGL
+            initialViewState={INITIAL_VIEW_STATE}
+            controller={true}
+            layers={layers} >
+            <MapView id="map" width="50%" controller={true}>
+                <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+            </MapView>
+            <FirstPersonView width="50%" x="50%" fovy={50} />
+        </DeckGL>
+    )    
+
 }
 
 export { App }
