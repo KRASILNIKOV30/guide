f4e8ca90a1e181307e7876c6aa340029b86ccf64
Safarov_Tahir <salarovsmv@gmail.com>
2022-02-01 23:27:19 +0300

    Попытался вставить слой Path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import DeckGL from '../node_modules/@deck.gl/react';
-import {LineLayer} from '../node_modules/@deck.gl/layers';
+import {LineLayer, PathLayer} from '../node_modules/@deck.gl/layers';
 import {StaticMap} from 'react-map-gl';
 import {MapView, FirstPersonView} from '../node_modules/@deck.gl/core';
 
@@ -13,21 +13,44 @@ const INITIAL_VIEW_STATE = {
 
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoia3Jhc2lsbmlrb3YzMCIsImEiOiJja3owODh1OGwwMjE2MnBwcXlkNGpwZ281In0.Ep9Yn349-UgsCl8SPEroHQ';
 
-const data = [
-    {sourcePosition: [56.6418, 47.8896], targetPosition: [56.6418, 47.8896]}
+const dataLine = [
+    {
+        sourcePosition: [56.6418, 47.8896],
+        targetPosition: [56.6418, 47.8896]
+    }
 ];
+
+const dataPath = [
+    {
+        path: [[56.6418, 47.8896], [57.0000, 48.0000], [56.0000, 47.0000]],
+        name: 'Test route',
+        color: [255, 0, 0]
+    }
+]
+
+type dataPathType = typeof dataPath[0];
 
 const App = () => {
     const layers = [
-        new LineLayer({id: 'line-layer', data})
+        new LineLayer({id: 'line-layer', dataLine}),
+        new PathLayer({
+            id: 'path-layer',
+            dataPath,
+            pickable: true,
+            widthScale: 10,
+            widthMinPixels: 20,
+            getPath: (d: dataPathType) => d.path
+        })
       ];
     
     return ( 
         <DeckGL
             initialViewState={INITIAL_VIEW_STATE}
             controller={true}
-            layers={layers} >
-            <MapView id="map" width="50%" controller={true}>
+            layers={layers}
+            getTooltip={(object: dataPathType) => object && object.name}
+        >
+            <MapView id="map" width="100%" controller={true}>
                 <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
             </MapView>
             <FirstPersonView width="50%" x="50%" fovy={50} />
