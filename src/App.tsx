f533b9779f676c2cf326c30269716971940d45ac
Krasilnikov30 <79829503+KRASILNIKOV30@users.noreply.github.com>
2022-02-18 23:01:24 +0300

    Написал curl запрос

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,8 @@ const INITIAL_VIEW_STATE = {
     bearing: 0
 };
 
+//curl 'http://router.project-osrm.org/route/v1/driving/56.6418,47.889690;56.641890,47.879654;56.741888,47.889612?overview=false'
+
 const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1Ijoia3Jhc2lsbmlrb3YzMCIsImEiOiJja3owODh1OGwwMjE2MnBwcXlkNGpwZ281In0.Ep9Yn349-UgsCl8SPEroHQ';
 
 const data = [
@@ -27,7 +29,7 @@ const App = () => {
             initialViewState={INITIAL_VIEW_STATE}
             controller={true}
             layers={layers} >
-            <MapView id="map" width="50%" controller={true}>
+            <MapView id="map" width="100%" controller={true}>
                 <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
             </MapView>
             <FirstPersonView width="50%" x="50%" fovy={50} />
