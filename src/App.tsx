2f423bfad4836520aa57c7b955a5ec4284f54b75
Krasilnikov30 <79829503+KRASILNIKOV30@users.noreply.github.com>
2022-04-03 19:22:57 +0300

    Добавил кнопку "Мое местоположение"

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { YMaps, Map } from 'react-yandex-maps';
+import { YMaps, Map, GeolocationControl } from 'react-yandex-maps';
+import './App.css';
 
 const App = () => {
     return (
         <YMaps>
             <Map
                 defaultState = {{ 
-                    center: [55.75, 37.57], 
-                    zoom: 9,
+                    center: [56.64, 47.89], 
+                    zoom: 13,
                     type: 'yandex#hybrid',  
                 }}
-            />
+                className = 'map'
+            >
+                <GeolocationControl options={{ float: 'right' }} />
+            </Map>
         </YMaps>
     )
 }
