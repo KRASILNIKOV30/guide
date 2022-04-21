ba55cb66430a18f42e52300e7e400a273e0508c6
Krasilnikov30 <salarovsmv@gmail.com>
2022-04-21 17:33:20 +0300

    Редьюсеры Тахира

--- src/model/reducers.ts
+++ src/model/reducers.ts
@@ -7,4 +7,21 @@ function selectTourReducer(userData: userData, tourId: string): userData {
     return newUserData
 }
 
+function startStopRouteReducer(userData: userData, started: boolean): userData {
+    const newUserData = deepClone(userData) as userData;
+    newUserData.started = started;
+    return newUserData
+}
+
+function completeToureReducer(userData: userData): userData {
+    const newUserData = deepClone(userData) as userData;
+    if(newUserData.selectedTourId !== undefined && !newUserData.completedTouresId.includes(newUserData.selectedTourId)) {
+        newUserData.completedTouresId.push(newUserData.selectedTourId)
+    }
+    newUserData.selectedTourId = undefined;
+    newUserData.routeState = [];
+    newUserData.started = false;
+    return newUserData
+}
+
 export { selectTourReducer }
