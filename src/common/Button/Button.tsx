15bb01f1043d479dd653a8c46f364a8178d97360
Safarov_Tahir <salarovsmv@gmail.com>
2022-05-10 09:25:31 +0300

    Исправил верстку добавил кнопку

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -3,11 +3,12 @@ import styles from './Button.module.css';
 import { Link } from 'react-router-dom'
 
 interface ButtonProps {
-    viewStyle: 'main' | 'secondary' | 'add' | 'delete' | 'delete_outline'
+    viewStyle: 'main' | 'secondary' | 'with_image' | 'add' | 'delete' | 'delete_outline'
             | 'hint' | 'back' | 'cancel' | 'location' | 'plus' | 'minus' | 'back_to_location'
             | 'info' | 'info_white',
     text?: string,
     to?: string,
+    image?: string,
     onClick: () => void,
     className?: string
 }
@@ -16,6 +17,7 @@ const Button = ({
     viewStyle,
     text = '',
     to,
+    image = '',
     onClick,
     className = ''
 }: ButtonProps) => {
@@ -23,6 +25,7 @@ const Button = ({
     switch(viewStyle) {
         case 'main': {buttonStyle = styles.main; break}
         case 'secondary': {buttonStyle = styles.secondary; break}
+        case 'with_image': {buttonStyle = styles.with_image; break}
         case 'add': {buttonStyle = styles.add; break}
         case 'delete': {buttonStyle = styles.delete; break}
         case 'delete_outline': {buttonStyle = styles.delete_outline; break}
@@ -42,9 +45,12 @@ const Button = ({
             className = {`${styles.button} ${buttonStyle} ${className}`}
             onClick = {onClick}
         >
-            <div className = {(viewStyle === 'main' || viewStyle === 'secondary') ? viewStyle === 'main' ? styles.text_container_gradient : styles.text_container : styles.text_container_hide}>
+            <div className = {(viewStyle === 'main' || viewStyle === 'secondary' || viewStyle === 'with_image') ? viewStyle === 'main' ? styles.text_container_gradient : styles.text_container : styles.text_container_hide}>
                 {
                     to !== undefined ? <Link to={to}>{text}</Link> : <>{text}</>
+                }
+                {
+                    (viewStyle === "with_image")? <div className={styles.image} style={{"backgroundImage": `url(${image})`}}> </div> : null
                 }
             </div>
         </button>
