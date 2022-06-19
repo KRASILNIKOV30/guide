7add92e4d6d0d4522d45281ddf82b8eacf27b79c
Krasilnikov30 <79829503+KRASILNIKOV30@users.noreply.github.com>
2022-06-19 15:02:47 +0300

    Заменили хук свайпа

--- src/routes/tourSelect/TourSelect.tsx
+++ src/routes/tourSelect/TourSelect.tsx
@@ -5,8 +5,9 @@ import { AppDispatch } from '../../model/store';
 import { selectTour } from '../../model/actionCreators';
 import { State } from '../../model/types';
 import { useRef, useState } from 'react';
-import { useSwipe } from '../../core/hooks/useSwipe';
 import { useNavigate } from 'react-router-dom';
+import { useSwipeable } from 'react-swipeable';
+import { config } from 'process';
 
 type TourInfo = {
     id: string;
@@ -30,35 +31,44 @@ const TourSelect = ({ tours, selectTour }: ToolBarProps) => {
     const [focusedTourIndex, setFocusedTourIndex] = useState(0);
 
     const documentWidthRef = useRef(document.documentElement.clientWidth)
-    
+
     const silderRef = useRef<HTMLDivElement>(null);
 
-    useSwipe({
-        elementRef: silderRef,
-        swipedRight: () => {
-            if (tours.length > focusedTourIndex + 1 && silderRef.current) {
-                setFocusedTourIndex(focusedTourIndex + 1);
-            }
-        },
-        swipedLeft: () => {
-            if (focusedTourIndex > 0 && silderRef.current) {
-                setFocusedTourIndex(focusedTourIndex - 1);
-            }
+    const swipedLeft = () => {
+        if (tours.length > focusedTourIndex + 1 && silderRef.current) {
+            setFocusedTourIndex(focusedTourIndex + 1);
         }
-    })
+    }
+
+    const swipedRight = () => {
+        if (focusedTourIndex > 0 && silderRef.current) {
+            setFocusedTourIndex(focusedTourIndex - 1);
+        }
+    }
+
+    const handlers = useSwipeable({
+        onSwipedRight: (eventData) => swipedRight(),
+        onSwipedLeft: (eventData) => swipedLeft(),
+        swipeDuration: 400,
+        delta: {
+            left: 40,
+            right: 40,
+        },
+        ...config,
+    });
 
     return (
         <div className={styles.tour_select}>
-            
+
             <div className={styles.header_container}>
                 <div className={styles.header_top}>
                     <h1 className={styles.header_text}> Туры от Гида </h1>
-                    <Button viewStyle='info' onClick={() => {}}/>
+                    <Button viewStyle='info' onClick={() => { }} />
                 </div>
                 <div className={styles.header_bot}>
                     <div className={styles.placemark_image}></div>
                     <div className={styles.town_name}> Йошкар-Ола </div>
-                </div>                       
+                </div>
             </div>
 
             <div className={styles.tour_info_container}>
@@ -66,44 +76,47 @@ const TourSelect = ({ tours, selectTour }: ToolBarProps) => {
                 <h3 className={styles.tour_info}>{tours[focusedTourIndex].description}</h3>
             </div>
 
-            <div
-                className={styles.tour_images_container}
-                ref = {silderRef}
-                style = {{
-                    "width": `${documentWidthRef.current * tours.length}px`,
-                    "transform": `translate(${focusedTourIndex * -78}vw, 0)`,
-                }}
-            >
-                {
-                    tours.map((tour) => 
-                        <div 
-                            className={`${styles.tour_image} ${(tours.findIndex(tourI => tourI.id === tour.id) === focusedTourIndex) && styles.tour_image_active}`}
-                            style = {{"backgroundImage": `url(${tour.image})`}}
-                            key = {tour.id}
-                            onClick = {() => {
-                                if (tour.id === tours[focusedTourIndex].id) {
+            <div {...handlers}>
+                <div
+                    className={styles.tour_images_container}
+                    ref={silderRef}
+                    style={{
+                        "width": `${documentWidthRef.current * tours.length}px`,
+                        "transform": `translate(${focusedTourIndex * -78}vw, 0)`,
+                    }}
+                >
+                    {
+                        tours.map((tour) =>
+                            <div
+                                className={`${styles.tour_image} ${(tours.findIndex(tourI => tourI.id === tour.id) === focusedTourIndex) && styles.tour_image_active}`}
+                                style={{ "backgroundImage": `url(${tour.image})` }}
+                                key={tour.id}
+                                onClick={() => {
+                                    if (tour.id === tours[focusedTourIndex].id) {
+                                        selectTour(tours[focusedTourIndex].id);
+                                        navigate('/previewtour')
+                                    }
+                                }}
+                            >
+                                <Button text={tour.finished ? "Айда ещё!" : "Вперёд!"} viewStyle="main" to="/previewtour" onClick={() => {
                                     selectTour(tours[focusedTourIndex].id);
-                                    navigate('/previewtour')
+                                }} />
+                                {
+                                    tour.finished && <div className={styles.finished_mark}>Пройдено</div>
                                 }
-                            }}
-                        >
-                            <Button text={tour.finished? "Айда ещё!": "Вперёд!"} viewStyle="main" to="/previewtour" onClick={() => {
-                                selectTour(tours[focusedTourIndex].id);
-                            }} />
-                            {
-                                tour.finished && <div className={styles.finished_mark}>Пройдено</div>
-                            }
-                        </div>
-                    )
-                }
+                            </div>
+                        )
+                    }
+                </div>
             </div>
+            
 
             <div className={styles.slider_container}>
                 {
-                    tours.map((tour) => 
+                    tours.map((tour) =>
                         <div
                             className={`${styles.tour_point} ${(tours.findIndex(tourI => tourI.id === tour.id) === focusedTourIndex) && styles.tour_point_active}`}
-                            key = {tour.id}
+                            key={tour.id}
                         > </div>
                     )
                 }
@@ -121,7 +134,7 @@ const mapStateToProps = (state: State) => {
         image: tour.image,
         finished: state.userData.completedTouresId.includes(tour.id)
     }))
-    
+
     return {
         tours: tourInfo
     }
